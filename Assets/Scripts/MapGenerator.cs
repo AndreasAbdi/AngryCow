094eb1a38e214494517124beccfbd1144735d124
AndreasAbdi <andreasabdi@yahoo.ca>
2017-01-11 17:27:25 -0800

    Added the maptile prefab and map generator class

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Map
+{
+    [Range(1,100)]
+    public int height;
+    [Range(1, 100)]
+    public int width;
+    public GameObject mapTile;
+    public Coord CenterLocation;
+
+    public string seed;
+}
+
+[System.Serializable]
+public class Coord
+{
+    public int x;
+    public int y;
+}
+
+public class MapGenerator : MonoBehaviour {
+    public Map map;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
