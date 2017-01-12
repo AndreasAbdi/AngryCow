cc1deb0949807c49ae5cf68d94eda4d039a442a0
AndreasAbdi <andreasabdi@yahoo.ca>
2017-01-11 18:34:53 -0800

    Added spacing between objects and linked to a tile pool

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -41,20 +41,33 @@ public class MapGenerator : MonoBehaviour {
 
     void GenerateMap()
     {
+        ClearTilePool();
+        CreateMap();
+    }
+
+    void CreateMap()
+    {
         (
         from x in Enumerable.Range(0, map.width)
         from y in Enumerable.Range(0, map.height)
         select new Coord(x, y)
-        )
-        .ToList()
-        .ForEach(mapCoord => {
-            Vector3 tilePosition = new Vector3(-map.width/2 + 0.5f + mapCoord.x, 0, -map.height/2 + 0.5f + mapCoord.y);
-            GameObject newTile = Instantiate(mapTile, tilePosition, Quaternion.identity) as GameObject;
-            Transform tileTransform = newTile.GetComponent<Transform>();
+        ).ToList()
+        .Select(mapCoord => {
+            Vector3 tilePosition = new Vector3(-map.width / 2 + 0.5f + mapCoord.x, 0, -map.height / 2 + 0.5f + mapCoord.y);
+            return Instantiate(mapTile, tilePosition, Quaternion.identity) as GameObject;
+        }).ToList()
+        .ForEach(tile => {
+            Transform tileTransform = tile.GetComponent<Transform>();
             tileTransform.localScale = Vector3.one * (1 - map.outlineSize);
             tileTransform.SetParent(tilePool);
         });
+    }
 
-       
+    void ClearTilePool()
+    {
+        while (tilePool.childCount > 0)
+        {
+            DestroyImmediate(tilePool.GetChild(0));
+        }
     }
 }
