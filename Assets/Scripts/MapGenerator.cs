0f56a9c0c797d65e91a3a10cf5d9bfa2a3488d40
AndreasAbdi <andreasabdi@yahoo.ca>
2017-01-12 19:51:42 -0800

    Added navmesh and border masks

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -11,11 +11,22 @@ public class Map
     public int height;
     [Range(1, 100)]
     public int width;
+
     [Range(0, 1)]
     public float outlineSize;
+
+    [Range(0, 1)]
+    public float obstaclePercent;
+
+    public float minObstacleHeight;
+    public float maxObstacleHeight;
+    public float foregroundColor;
+    public float backgroundColor;
+
+    public string seed;
+
     [HideInInspector]
     public Coord centerLocation;
-    public string seed;
 }
 
 [System.Serializable]
@@ -42,14 +53,21 @@ public class Coord
 }
 
 public class MapGenerator : MonoBehaviour {
-    [Range(0,1)]
-    public float obstaclePercent;
+    public Map[] maps;
+    public int currentMapIndex;
+
     public Transform obstaclePrefab;
-    public Map map;
-    public float tileSize;
     public Transform mapTile;
     public Transform objectPool;
+    [Range(0.001f, 100)]
+    public float tileSize;
 
+    //for controlling the navigation mesh. 
+    public Transform navMeshFloor;
+    public Transform navMeshMaskPrefab;
+    public Vector2 maxMapSize;
+
+    Map map;
 
     void Start () {
         GenerateMap();
@@ -57,9 +75,34 @@ public class MapGenerator : MonoBehaviour {
 
     public void GenerateMap()
     {
+        SetCurrentMap();
         ClearTilePool();
         CreateMap();
         CreateObstacles();
+        UpdateNavMesh();
+    }
+
+    void SetCurrentMap()
+    {
+        map = maps[currentMapIndex];
+    }
+
+    void CreateMap()
+    {
+        (
+        from x in Enumerable.Range(0, map.width)
+        from y in Enumerable.Range(0, map.height)
+        select new Coord(x, y)
+        ).ToList()
+        .Select(mapCoord => {
+            Vector3 tilePosition = CoordToVector3(mapCoord);
+            return Instantiate(mapTile, tilePosition, Quaternion.identity) as GameObject;
+        }).ToList()
+        .ForEach(tile => {
+            Transform tileTransform = tile.GetComponent<Transform>();
+            tileTransform.localScale = Vector3.one * (1 - map.outlineSize) * tileSize;
+            tileTransform.SetParent(objectPool);
+        });
     }
 
     void CreateObstacles()
@@ -74,35 +117,60 @@ public class MapGenerator : MonoBehaviour {
         bool[,] obstacleMap = new bool[map.width, map.height];
         map.centerLocation = new Coord(map.width / 2, map.height / 2);
 
-        int obstacleCount = (int) (map.width * map.height * obstaclePercent);
+        int obstacleCount = (int)(map.width * map.height * map.obstaclePercent);
         int currentObstacleCount = 0;
         Enumerable
             .Range(0, obstacleCount)
             .Select(
-                (obstacleNumber) =>{
-                Coord nextCoord = shuffledCoords.Dequeue();
-                shuffledCoords.Enqueue(nextCoord);
-                return nextCoord;
-            })
+                (obstacleNumber) => {
+                    Coord nextCoord = shuffledCoords.Dequeue();
+                    shuffledCoords.Enqueue(nextCoord);
+                    return nextCoord;
+                })
             .ToList()
             .ForEach(
                 (position) => {
-                currentObstacleCount++;
-                obstacleMap[position.x, position.y] = true;
-                if(position != map.centerLocation && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
-                {
-                    float targetScale =  (1 - map.outlineSize) * tileSize;
-                    Vector3 upShift = Vector3.up * targetScale * 0.5f;
-                    Transform obstacle = Instantiate(obstaclePrefab, CoordToVector3(position) + upShift, Quaternion.identity) as Transform;
-                    obstacle.parent = objectPool;
-                    obstacle.localScale = Vector3.one * targetScale;
-                }
+                    currentObstacleCount++;
+                    obstacleMap[position.x, position.y] = true;
+                    if (position != map.centerLocation && MapIsFullyAccessible(obstacleMap, currentObstacleCount))
+                    {
+                        float targetScale = (1 - map.outlineSize) * tileSize;
+                        Vector3 upShift = Vector3.up * targetScale * 0.5f;
+                        Transform obstacle = Instantiate(obstaclePrefab, CoordToVector3(position) + upShift, Quaternion.identity) as Transform;
+                        obstacle.parent = objectPool;
+                        obstacle.localScale = Vector3.one * targetScale;
+                    }
                     else
-                {
-                    obstacleMap[position.x, position.y] = false;
-                    currentObstacleCount--;
-                }
-            });
+                    {
+                        obstacleMap[position.x, position.y] = false;
+                        currentObstacleCount--;
+                    }
+                });
+    }
+
+    void UpdateNavMesh()
+    {
+        navMeshFloor.localScale = new Vector3(maxMapSize.x, maxMapSize.y) * tileSize;
+        float positionScale = 1/(4 * tileSize);
+        float verticalPositionScale = (map.width + maxMapSize.x) * positionScale;
+        float horizontalPositionScale = (map.height + maxMapSize.y) * positionScale;
+        Vector3 partialLocalScale = new Vector3((maxMapSize.x - map.width) / 2, 1, map.height) * tileSize;
+        Vector3 fillingLocalScale = new Vector3(maxMapSize.x, 1, (maxMapSize.y - map.height) / 2) * tileSize;
+        //left 
+        CreateNavMeshMask(Vector3.left * verticalPositionScale, partialLocalScale);
+        //right
+        CreateNavMeshMask(Vector3.right * verticalPositionScale, partialLocalScale);
+        //top
+        CreateNavMeshMask(Vector3.forward * horizontalPositionScale, fillingLocalScale);
+        //bottom
+        CreateNavMeshMask(Vector3.back * horizontalPositionScale, fillingLocalScale);
+    }
+
+    void CreateNavMeshMask(Vector3 position, Vector3 localScale)
+    {
+        Transform navMeshMask = Instantiate(navMeshMaskPrefab, position, Quaternion.identity) as Transform;
+        navMeshMask.parent = objectPool;
+        navMeshMask.localScale = localScale;
     }
     
     Vector3 CoordToVector3(Coord coord)
@@ -149,24 +217,6 @@ public class MapGenerator : MonoBehaviour {
 
         int targetAccessibleTileCount = (int)(map.width * map.height - currentObstacleCount);
         return targetAccessibleTileCount == accessibleTileCount;
-    }
-
-    void CreateMap()
-    {
-        (
-        from x in Enumerable.Range(0, map.width)
-        from y in Enumerable.Range(0, map.height)
-        select new Coord(x, y)
-        ).ToList()
-        .Select(mapCoord => {
-            Vector3 tilePosition = CoordToVector3(mapCoord);
-            return Instantiate(mapTile, tilePosition, Quaternion.identity) as GameObject;
-        }).ToList()
-        .ForEach(tile => {
-            Transform tileTransform = tile.GetComponent<Transform>();
-            tileTransform.localScale = Vector3.one * (1 - map.outlineSize) * tileSize;
-            tileTransform.SetParent(objectPool);
-        });
     }
 
     void ClearTilePool()
