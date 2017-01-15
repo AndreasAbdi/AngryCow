a4a02f321d334824bb94adfa912acf68be0bf96e
AndreasAbdi <andreasabdi@yahoo.ca>
2017-01-15 13:31:57 -0800

    Added player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody playerCharacter;
+    public float speed;
+
+    Vector3 movementInput;
+
+	void OnEnable()
+    {
+        movementInput = new Vector3();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f , Input.GetAxis("Vertical"));
+	}
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        playerCharacter.MovePosition(transform.position + movementInput * speed);
+    }
+}
