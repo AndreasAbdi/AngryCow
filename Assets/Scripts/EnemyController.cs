901db57ab74f70d1f8d8605f25170d78a09089bd
AndreasAbdi <andreasabdi@yahoo.ca>
2017-01-16 15:02:42 -0800

    Made the enemy move towards the player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour {
     public float speed;
     public Rigidbody rigidBody;
+    public NavMeshAgent navMeshAgent;
+    public GameObject player;
+
+    void Start()
+    {
+    }
 
     void FixedUpdate()
     {
-        Vector3 movement = Vector3.left * speed * Time.deltaTime;
-        rigidBody.MovePosition(transform.position + movement);    
+        navMeshAgent.destination = player.transform.position;
+
+        //Vector3 movement = Vector3.left * speed * Time.deltaTime;
+        //rigidBody.MovePosition(transform.position + movement);
     }
 }
